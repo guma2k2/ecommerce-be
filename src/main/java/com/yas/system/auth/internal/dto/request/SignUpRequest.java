200b33b452291c9e5329492730e1a87c66dfe517
Thuan Ngo | Dev <thuan.ngo@689cloud.asia>
2026-06-22 23:54:21 +0700

    Update: Handle reset password

--- src/main/java/com/yas/system/auth/internal/dto/request/SignUpRequest.java
+++ src/main/java/com/yas/system/auth/internal/dto/request/SignUpRequest.java
@@ -3,6 +3,7 @@ package com.yas.system.auth.internal.dto.request;
 import com.yas.system.common.response.ParamError;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record SignUpRequest(
         @NotBlank(message = ParamError.FIELD_NAME)
@@ -10,6 +11,10 @@ public record SignUpRequest(
         String email,
 
         @NotBlank(message = ParamError.FIELD_NAME)
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{8,}$",
+                message = "Password must have at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 special character and minimum 8 characters"
+        )
         String password,
 
         @NotBlank(message = ParamError.FIELD_NAME)
