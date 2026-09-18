c1659372a6f288682bc70d30c284f7b9a1766dbd
Thuan Ngo | Dev <thuan.ngo@689cloud.asia>
2026-09-18 23:15:27 +0700

    Update: Update response for product option api

--- src/main/java/com/yas/system/catalog/internal/dto/response/ProductOptionResponse.java
+++ src/main/java/com/yas/system/catalog/internal/dto/response/ProductOptionResponse.java
@@ -2,16 +2,20 @@ package com.yas.system.catalog.internal.dto.response;
 
 import com.yas.system.catalog.internal.entity.option.ProductOption;
 
-import java.util.List;
+import java.time.format.DateTimeFormatter;
 
 public record ProductOptionResponse(
         Long id,
-        String name
+        String name,
+        String createdAt,
+        String updatedAt
 ) {
     public static ProductOptionResponse from(ProductOption option) {
         return new ProductOptionResponse(
                 option.getId(),
-                option.getName()
+                option.getName(),
+                option.getCreatedAt() != null ? option.getCreatedAt().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME) : null,
+                option.getUpdatedAt() != null ? option.getUpdatedAt().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME) : null
         );
     }
 }
